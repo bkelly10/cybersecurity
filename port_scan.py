5c4b5586104bfef5f05b27ed1c02bdc2da62dc96
Bryan Kelly <bkelly@macbookair.myfiosgateway.com>
2025-09-13 23:48:56 -0400

    update

--- port_scan.py
+++ port_scan.py
@@ -2,17 +2,14 @@ import socket
 
 def scan_port(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
+    s.settimeout(2)
     result = s.connect_ex((host, port))
     s.close()
     return result
 
 host = "127.0.0.1"
-port = 80
 
-result = scan_port(host, port)
-
-if result == 0:
-    print(f"Port {port} is OPEN")
-else:
-    print(f"Port {port} is CLOSED")
+for port in range(1, 1025):
+    result = scan_port(host, port)
+    if result == 0:
+        print(f"Port {port} is OPEN")
