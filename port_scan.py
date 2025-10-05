db962705c0fa4c16bc273f4b00ddc335c5796435
bkelly10 <94399058+bkelly10@users.noreply.github.com>
2025-10-05 14:55:10 -0400

    Update port_scan.py
    added context

--- port_scan.py
+++ port_scan.py
@@ -1,5 +1,9 @@
 import socket 
 
+# Define a function that trys to connect to a specific host and port
+# Create a socket to connect to the port 
+# Set a timeout to make sure we don't freeze 
+# Try to connect to given host and port
 def scan_port(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
@@ -9,6 +13,8 @@ def scan_port(host, port):
 
 host = "127.0.0.1"
 
+# Loop through common / well known ports
+# Print the port as open if the conneciton was successful 
 for port in range(1, 1025):
     result = scan_port(host, port)
     if result == 0:
