a3b3f55c63dfd5378331795122a5aa259988beb3
bkelly10 <94399058+bkelly10@users.noreply.github.com>
2025-11-25 13:35:58 -0500

    Update smart_recon_scanner.py
    Added enumerate_services() to process open ports
    
    Added grab_ssh_banner() for SSH (port 22) enumeration
    
    Updated host from local 127.0.0.1 to router 192.168.1.1 to scan LAN device ports

--- smart_recon_scanner.py
+++ smart_recon_scanner.py
@@ -2,7 +2,7 @@ import socket
 
 def scan_port(host, port, timeout=2):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
+    s.settimeout(timeout)
     result = s.connect_ex((host, port))
     s.close()
     return result == 0
@@ -22,8 +22,23 @@ def scan_host(host, start_port, end_port):
 
 def enumerate_services(host, open_ports): 
     for port in open_ports:
-        print(f"[ENUM] ound open port {port}, checking service...")
+        print(f"[ENUM] Found open port {port}, checking service...")
+        if port == 22:
+            grab_ssh_banner(host, port)
 
-host = "127.0.0.1"
+def grab_ssh_banner(host, port=22, timeout=2):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        s.connect((host, port))
+        banner = s.recv(1024).decode(errors="ignore").strip()
+        print(f"[SSH] {host}:{port} banner: {banner}")
+    except Exception as e:
+        print(f"[SSH] Error grabbing banner from {host}:{port} - {e}")
+    finally:
+        s.close()
+
+host = "192.168.1.1"
 open_ports = scan_host(host, 1, 1024)
 enumerate_services(host, open_ports)
+
